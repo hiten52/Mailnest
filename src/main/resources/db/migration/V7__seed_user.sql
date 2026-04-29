8f7bd6dc73446e478c2480e1fbf5e2c2bc09acd7
Hiten <hitensolanki010@gmail.com>
2026-04-29 21:42:02 +0530

    Add migration to seed user

---
+++ src/main/resources/db/migration/V7__seed_user.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (user_id, username, password_hash)
+VALUES (
+    'ddf8994f-d522-4659-8d02-c1d479057be6',
+    'admin',
+    '$2a$10$nuKysNo2SIESEV4oT7xkmebId2Z977WPGhx5swk9XQhhTARUucy7e'
+);
